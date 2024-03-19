ad2f0bafa799cfc2a4e720b871d16e631c072565
niButera <nikolasxp683gmail.com>
2024-03-19 17:41:20 -0300

    relatorio medico | chamado arrumado

--- js/motivo.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("expandButton").addEventListener("click", function() {
-    console.log("Botão clicado!");
-    var expandedContent = document.getElementById("expandedContent");
-    if (expandedContent.style.display === "none") {
-        expandedContent.style.display = "block";
-    } else {
-        expandedContent.style.display = "none";
-    }
-});
